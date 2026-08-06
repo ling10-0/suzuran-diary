1a283f69d3512d3af778a5ba77f72a737c6c2521
ling10-0 <ling101208@gmail.com>
2026-08-07 00:32:21 +0800

    Apply new routes during Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import {routeTransform} from './scripts/route-transform.mjs';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [routeTransform(), react()],
   base: './'
 });
