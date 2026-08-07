1a421b653a11aef9263623643ecf0e558eb040ec
ling10-0 <ling101208@gmail.com>
2026-08-07 10:18:58 +0800

    Enable shared progress recovery during build

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import {routeTransform} from './scripts/route-transform.mjs';
 import {manuscriptTransform} from './scripts/manuscript-transform.mjs';
+import {syncTransform} from './scripts/sync-transform.mjs';
 
 export default defineConfig({
-  plugins: [routeTransform(), manuscriptTransform(), react()],
+  plugins: [routeTransform(), manuscriptTransform(), syncTransform(), react()],
   base: './'
 });
