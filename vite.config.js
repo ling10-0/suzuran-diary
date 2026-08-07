d88b202f957e29600799561fdd009e813c2a8ebe
ling10-0 <ling101208@gmail.com>
2026-08-07 11:28:05 +0800

    Enable first puzzle transform

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 import {routeTransform} from './scripts/route-transform.mjs';
 import {manuscriptTransform} from './scripts/manuscript-transform.mjs';
 import {syncTransform} from './scripts/sync-transform.mjs';
+import {firstPuzzleTransform} from './scripts/first-puzzle-transform.mjs';
 
 export default defineConfig({
-  plugins: [routeTransform(), manuscriptTransform(), syncTransform(), react()],
+  plugins: [routeTransform(), manuscriptTransform(), syncTransform(), firstPuzzleTransform(), react()],
   base: './'
 });
